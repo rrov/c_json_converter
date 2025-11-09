ea3c2df062c0842d8c2103d279a84da0fbe93bc3
rrov <developercrazyat@gmail.com>
2025-11-09 21:03:52 +0300

    parse int, find key

--- cjc.c
+++ cjc.c
@@ -6,25 +6,54 @@
 #include "cjc.h"
 
 
+static int find_json_key_substring(struct CJC_JsonKeySubstring *substring);
+static int parse_int_value(struct CJC_JsonKeySubstring *key_substring, int *value_destination);
+
+
 int cjc_parse_int(struct CJC_State *state, char *key, int *value_destination)
 {
-    size_t key_size = strlen(key);
+    struct CJC_JsonKeySubstring json_key_substring = {
+        .state = state,
+        .key = key,
+        .key_size = strlen(key),
+        .index = 0,
+        .shift = 0,
+    };
+
+    if (find_json_key_substring(&json_key_substring) != 0)
+    {
+        printf("Can not find key %s\n", key);
+        return 1;
+    }
+
+    printf("Key index: %lu; Key shift: %lu\n", json_key_substring.index, json_key_substring.shift);
+
+    return 0;
+}
+
+
+static int find_json_key_substring(struct CJC_JsonKeySubstring *substring)
+{
     size_t read_key_size = 0;
 
+    // TODO: rewrite to bits in byte
     bool read_key = false;
     bool skip_key = false;
     bool key_matched = false;
 
-    for (size_t i = 0; i < state->json_string_size; i++)
+    for (size_t i = 0; i < substring->state->json_string_size; i++)
     {
-        printf("%c", state->json_string[i]);
-
-        if (state->json_string[i] == '"')
+        if (substring->state->json_string[i] == '"')
         {
             if (read_key)
             {
-                printf("\nKey matched!\n");
+                substring->shift = (i - 1) - substring->index;
                 key_matched = true;
+                break;
+            } 
+            else
+            {
+                substring->index = i;
             }
 
             read_key = !read_key;
@@ -37,9 +66,9 @@ int cjc_parse_int(struct CJC_State *state, char *key, int *value_destination)
             continue;
         }
 
-        if (read_key && read_key_size < key_size)
+        if (read_key && read_key_size < substring->key_size)
         {
-            if (state->json_string[i] != key[read_key_size])
+            if (substring->state->json_string[i] != substring->key[read_key_size])
             {
                 skip_key = true;
                 read_key_size = 0;
@@ -48,7 +77,11 @@ int cjc_parse_int(struct CJC_State *state, char *key, int *value_destination)
             read_key_size++;
         }
     }
-    printf("\n");
 
+    return 0;
+}
+
+static int parse_int_value(struct CJC_JsonKeySubstring *key_substring, int *value_destination)
+{
     return 0;
 }
--- cjc.h
+++ cjc.h
@@ -10,6 +10,17 @@ struct CJC_State
     const size_t json_string_size;
 };
 
+struct CJC_JsonKeySubstring
+{
+    struct CJC_State *state;
+    
+    const char *key;
+    const size_t key_size;
+    
+    size_t index;
+    size_t shift;
+};
+
 int cjc_parse_int(struct CJC_State *state, char *key, int *value_destination);
 
 #endif
