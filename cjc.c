538fc3c8daca234bb999bc679f903f0b16ce515d
cogito <developercrazyat@gmail.com>
2026-01-03 11:14:29 +0300

    0.0.0

--- cjc.c
+++ cjc.c
@@ -1,6 +1,22 @@
 #include "cjc.h"
 
+static unsigned char character_is_syntax_character(char *character)
+{
+    switch (*character)
+    {
+        case '{':
+        case '}':
+        case '[':
+        case ']':
+        case ':':
+        case ',':
+        case '"': return 1;
+        default: return 0;
+    }
+}
+
 enum CJC_Result cjc_cursor_move_inside(struct CJC_Cursor *cursor)
 {
+    while (cursor->json[cursor->index] != '{') ++cursor->index;
     return CJC_RESULT_SUCCESS;
 }
