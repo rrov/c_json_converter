adb4bbb62cad1a02c69218217fb98cf3c0cb05f2
rrov <developercrazyat@gmail.com>
2026-01-17 10:20:57 +0300

    cjc_cursor_move_forward

--- cjc.c
+++ cjc.c
@@ -2,22 +2,7 @@
 #include <stddef.h>
 
 
-static unsigned char character_is_syntax_character(char *character)
-{
-    switch (*character)
-    {
-        case '{':
-        case '}':
-        case '[':
-        case ']':
-        case ':':
-        case ',':
-        case '"': return 1;
-        default: return 0;
-    }
-}
-
-static unsigned char cursor_on_escaped_character(struct CJC_Cursor *cursor, char character)
+static cjc_bool cursor_on_escaped_character(struct CJC_Cursor *cursor, char character)
 {
     return cursor->index != 0 && cursor->json[cursor->index] == character && cursor->json[cursor->index - 1] == '\\';
 }
@@ -50,9 +35,9 @@ enum CJC_Result cjc_cursor_move_inside(struct CJC_Cursor *cursor)
 
 enum CJC_Result cjc_cursor_move_outside(struct CJC_Cursor *cursor)
 {
-    size_t square_brackets_count = 0;
-    size_t curly_brackers_count  = 0;
-    cjc_bool inside_quote = 0;
+    size_t square_brackets_count    = 0;
+    size_t curly_brackers_count     = 0;
+    cjc_bool inside_quote           = 0;
 
     if (cursor->index > 0)
     {
@@ -65,7 +50,8 @@ enum CJC_Result cjc_cursor_move_outside(struct CJC_Cursor *cursor)
             inside_quote                != 0    ||
             square_brackets_count       != 0    ||
             curly_brackers_count        != 0
-        ) ||
+        ) 
+        ||
         (
             cursor->json[cursor->index] != '{'  &&
             cursor->json[cursor->index] != '['  &&
@@ -93,6 +79,42 @@ enum CJC_Result cjc_cursor_move_outside(struct CJC_Cursor *cursor)
 
 enum CJC_Result cjc_cursor_move_forward(struct CJC_Cursor *cursor)
 {
-    
+    size_t      square_brackets_count   = 0;
+    size_t      curly_brackers_count    = 0;
+    cjc_bool    inside_quote            = 0;
+    cjc_bool    after_comma             = 0;
+
+    while 
+    (
+        (
+            inside_quote            == 0 && 
+            after_comma             == 0 && 
+            cursor->json[cursor->index] != '\0'
+        )
+    )
+    {
+
+        if (inside_quote == 0)
+        {
+            if (cursor->json[cursor->index] == '}') ++curly_brackers_count;
+            if (cursor->json[cursor->index] == ']') ++square_brackets_count;
+            if (cursor->json[cursor->index] == '{') --curly_brackers_count;
+            if (cursor->json[cursor->index] == '[') --square_brackets_count;
+        }
+
+        /* Each stop of cursor should be outside any quote */
+        if (cursor->json[cursor->index] == '"' && !cursor_on_escaped_character(cursor, '"'))
+            inside_quote = inside_quote > 0 ? 0 : 1;
+
+        
+
+        if      (inside_quote == 1)                     after_comma = 0;
+        else if (cursor->json[cursor->index] == ',')    after_comma = 1;
+
+        ++cursor->index;
+    }
+
+    if (cursor->json[cursor->index] == '\0') return CJC_END_OF_JSON;
+
     return CJC_RESULT_SUCCESS;
 }
