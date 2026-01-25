ba6e6eab795c4de826033e4125d4905454f113ac
rrov <developercrazyat@gmail.com>
2026-01-25 09:44:22 +0300

    test.c

--- test.c
+++ test.c
@@ -30,160 +30,118 @@ void test_status_print(char *test_name, cjc_bool fail)
 
 void cjc_cursor_move_inside_test_1(void)
 {
-    enum CJC_RESULT result;
-    struct CJC_Cursor cursor;
+    enum CJC_RESULT     result;
+    struct CJC_Cursor   cursor;
+    cjc_bool            fail;
 
-    cursor.json = json_example_1;
-    cursor.index = 0;
+    cursor.json         = json_example_1;
+    cursor.index        = 0;
+    fail                = 0;
 
-    result = cjc_cursor_move_inside(&cursor);
-    
-    printf("cjc_cursor_move_inside_test_1: ");
-    printf("result: %2d, cursor index: %4lu\n", result, cursor.index);
+    if ((result = cjc_cursor_move_inside(&cursor))) result_error_print(&result, &fail);
+    if (cursor.index != 1) cursor_index_error_print(&cursor, &fail, 1);
 
-    if (cursor.index != 1)
-    {
-        printf("ERROR: cursor index should be 1\n");
-    }
+    test_status_print("cjc_cursor_move_inside_test_1", fail);
 }
 
 void cjc_cursor_move_inside_test_2(void)
 {
-    enum CJC_RESULT result;
-    struct CJC_Cursor cursor;
+    enum CJC_RESULT     result;
+    struct CJC_Cursor   cursor;
+    cjc_bool            fail;
 
-    cursor.json = json_example_1;
-    cursor.index = 0;
+    cursor.json         = json_example_1;
+    cursor.index        = 0;
+    fail                = 0;
 
-    result = cjc_cursor_move_inside(&cursor);
-    result = cjc_cursor_move_inside(&cursor);
+    if ((result = cjc_cursor_move_inside(&cursor))) result_error_print(&result, &fail);
+    if ((result = cjc_cursor_move_inside(&cursor)) != CJC_END_OF_JSON) result_error_print(&result, &fail);
     
-    printf("cjc_cursor_move_inside_test_2: ");
-    printf("result: %2d, cursor index: %4lu\n", result, cursor.index);
-
-    if (result != CJC_END_OF_JSON)
-    {
-        printf("ERROR: result should be CJC_END_OF_JSON\n");
-    }
+    test_status_print("cjc_cursor_move_inside_test_2", fail);
 }
 
 void cjc_cursor_move_inside_test_3(void)
 {
-    enum CJC_RESULT result;
-    struct CJC_Cursor cursor;
+    enum CJC_RESULT     result;
+    struct CJC_Cursor   cursor;
+    cjc_bool            fail;
 
-    cursor.json = json_example_2;
-    cursor.index = 0;
+    cursor.json         = json_example_2;
+    cursor.index        = 0;
+    fail                = 0;
 
-    result = cjc_cursor_move_inside(&cursor);
-    
-    printf("cjc_cursor_move_inside_test_3: ");
-    printf("result: %2d, cursor index: %4lu\n", result, cursor.index);
+    if ((result = cjc_cursor_move_inside(&cursor))) result_error_print(&result, &fail);
+    if (cursor.index != 1) cursor_index_error_print(&cursor, &fail, 1);
 
-    if (cursor.index != 1)
-    {
-        printf("ERROR: cursor index should be 1\n");
-    }
+    test_status_print("cjc_cursor_move_inside_test_3", fail);
 }
 
 void cjc_cursor_move_inside_test_4(void)
 {
-    enum CJC_RESULT result;
-    struct CJC_Cursor cursor;
+    enum CJC_RESULT     result;
+    struct CJC_Cursor   cursor;
+    cjc_bool            fail;
 
-    cursor.json = json_example_2;
-    cursor.index = 0;
+    cursor.json         = json_example_2;
+    cursor.index        = 0;
+    fail                = 0;
 
-    result = cjc_cursor_move_inside(&cursor);
-    result = cjc_cursor_move_inside(&cursor);
-
-    printf("cjc_cursor_move_inside_test_4: ");
-    printf("result: %2d, cursor index: %4lu\n", result, cursor.index);
-
-    if (result != CJC_END_OF_JSON)
-    {
-        printf("ERROR: result should be CJC_END_OF_JSON\n");
-    }
+    if ((result = cjc_cursor_move_inside(&cursor))) result_error_print(&result, &fail);
+    if ((result = cjc_cursor_move_inside(&cursor)) != CJC_END_OF_JSON) result_error_print(&result, &fail);
+    
+    test_status_print("cjc_cursor_move_inside_test_4", fail);
 }
 
 void cjc_cursor_move_outside_test_1(void)
 {
-    enum CJC_RESULT result;
-    struct CJC_Cursor cursor;
+    enum CJC_RESULT     result;
+    struct CJC_Cursor   cursor;
+    cjc_bool            fail;
 
-    cursor.json = json_example_3;
-    cursor.index = 0;
+    cursor.json         = json_example_3;
+    cursor.index        = 0;
+    fail                = 0;
 
-    result = cjc_cursor_move_inside(&cursor);
-    result = cjc_cursor_move_inside(&cursor);
+    if ((result = cjc_cursor_move_inside(&cursor))) result_error_print(&result, &fail);
+    if ((result = cjc_cursor_move_inside(&cursor))) result_error_print(&result, &fail);
 
-    printf("cjc_cursor_move_outside_test_1: ");
-    printf("result: %2d, cursor index: %4lu\n", result, cursor.index);
+    if (cursor.index != 11) cursor_index_error_print(&cursor, &fail, 11);
 
-    if (cursor.index != 11)
-    {
-        printf("ERROR: cursor index should be 11, got: %lu\n", cursor.index);
-    }
-
-    result = cjc_cursor_move_outside(&cursor);
+    if ((result = cjc_cursor_move_outside(&cursor))) result_error_print(&result, &fail);
     
-    printf("cjc_cursor_move_outside_test_1: ");
-    printf("result: %2d, cursor index: %4lu\n", result, cursor.index);
+    if (cursor.index != 10) cursor_index_error_print(&cursor, &fail, 10);
 
-    if (cursor.index != 10)
-    {
-        printf("ERROR: cursor index should be 10, got: %lu\n", cursor.index);
-    }
+    if ((result = cjc_cursor_move_outside(&cursor))) result_error_print(&result, &fail);
 
-    result = cjc_cursor_move_outside(&cursor);
+    if (cursor.index != 0) cursor_index_error_print(&cursor, &fail, 0);
 
-    printf("cjc_cursor_move_outside_test_1: ");
-    printf("result: %2d, cursor index: %4lu\n", result, cursor.index);
-
-    if (cursor.index != 0)
-    {
-        printf("ERROR: cursor index should be 0, got: %lu\n", cursor.index);
-    }
+    test_status_print("cjc_cursor_move_outside_test_1", fail);
 }
 
 void cjc_cursor_move_outside_test_2(void)
 {
-    enum CJC_RESULT result;
-    struct CJC_Cursor cursor;
+    enum CJC_RESULT     result;
+    struct CJC_Cursor   cursor;
+    cjc_bool            fail;
 
-    cursor.json = json_example_4;
-    cursor.index = 0;
+    cursor.json         = json_example_4;
+    cursor.index        = 0;
+    fail                = 0;
 
-    result = cjc_cursor_move_inside(&cursor);
-    result = cjc_cursor_move_inside(&cursor);
+    if ((result = cjc_cursor_move_inside(&cursor))) result_error_print(&result, &fail);
+    if ((result = cjc_cursor_move_inside(&cursor))) result_error_print(&result, &fail);
 
-    printf("cjc_cursor_move_outside_test_2: ");
-    printf("result: %2d, cursor index: %4lu\n", result, cursor.index);
+    if (cursor.index != 16) cursor_index_error_print(&cursor, &fail, 16);
 
-    if (cursor.index != 16)
-    {
-        printf("ERROR: cursor index should be 16, got: %lu\n", cursor.index);
-    }
-
-    result = cjc_cursor_move_outside(&cursor);
+    if ((result = cjc_cursor_move_outside(&cursor))) result_error_print(&result, &fail);
     
-    printf("cjc_cursor_move_outside_test_2: ");
-    printf("result: %2d, cursor index: %4lu\n", result, cursor.index);
+    if (cursor.index != 15) cursor_index_error_print(&cursor, &fail, 15);
 
-    if (cursor.index != 15)
-    {
-        printf("ERROR: cursor index should be 15, got: %lu\n", cursor.index);
-    }
+    if ((result = cjc_cursor_move_outside(&cursor))) result_error_print(&result, &fail);
 
-    result = cjc_cursor_move_outside(&cursor);
+    if (cursor.index != 0) cursor_index_error_print(&cursor, &fail, 0);
 
-    printf("cjc_cursor_move_outside_test_2: ");
-    printf("result: %2d, cursor index: %4lu\n", result, cursor.index);
-
-    if (cursor.index != 0)
-    {
-        printf("ERROR: cursor index should be 0, got: %lu\n", cursor.index);
-    }
+    test_status_print("cjc_cursor_move_outside_test_2", fail);
 }
 
 void cjc_cursor_move_forward_test_1(void)
